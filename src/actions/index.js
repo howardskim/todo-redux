79dc2b03a545e24af3318953cc9ad9545fa46f84
Howard Kim <howardkim820@gmail.com>
2018-08-23 15:53:30 -0700

    displaying complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
 import types from './types';
+import axios from 'axios'
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=thisismyapikey_69';
 
-const API_KEY = '?key=thisismyapikey_69'
+//just say export because we are gonna export alot of function
+
+export function getAllListData(){
+    const response = axios.get(`${BASE_URL}/todos/${API_KEY}`);
+    return {
+        type: types.GET_ALL_LIST_DATA,
+        payload: response
+        
+        //All we are doing is just Setting a string to this type property
+    }
+}
