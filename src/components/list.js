17d57d28d11825104743768dbc208d9a4001d0cc
Howard Kim <howardkim820@gmail.com>
2018-08-23 12:58:00 -0700

    routing done

---
+++ src/components/list.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class List extends Component{
+    render(){
+        return(
+            <ul className="collection">
+                <li className="collection-item">
+                    My first list Item
+                </li>
+            </ul>
+        )
+    }
+}
+
+export default List;
